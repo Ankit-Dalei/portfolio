3d2f7f2d6f4000f0400abcbaaea20caee95f628f
Ankit-Dalei <ankitdalei123@gmail.com>
2024-09-10 11:06:04 +0530

    first commit

---
+++ src/pages/App.jsx
@@ -0,0 +1,24 @@
+import Header from '../sections/Header'
+import Hero from '../sections/Hero'
+import Skills from '../sections/Skills'
+import Projects from '../sections/Projects'
+import Footer from '../sections/Footer'
+import AboutMe from '../sections/AboutMe'
+
+function App() {
+
+  return (
+    <div>
+      <Header />
+      <Hero />
+      <Skills />
+      <AboutMe />
+      <Projects />
+      <AboutMe />
+      <Projects />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
