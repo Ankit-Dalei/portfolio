e5eb99837e8571c418e95c6c78205b9c1294cde4
Ankit-Dalei <ankitdalei123@gmail.com>
2025-05-26 03:22:22 +0530

    test up to photo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,9 @@
-import { Jacques_Francois_Shadow } from "next/font/google";
+import { Jacques_Francois_Shadow , Italiana } from "next/font/google";
+
+const italiana = Italiana({
+  subsets: ['latin'],
+  weight: '400',
+});
 
 const jacques = Jacques_Francois_Shadow({
   subsets: ['latin'],
@@ -18,11 +23,45 @@ export default function Home() {
               Ankit Dalei
             </div>
           </div>
-          <div className="bg-amber-600 mt-1 w-full h-[16vh]">
-
+          <div className=" mt-1 w-full h-[16vh]">
+            <div className="w-full flex flex-col items-center justify-center">
+              <div className="w-full h-[8px] bg-[#5C724A]"></div>
+              <div className="w-full h-[4px] bg-[#5C724A] mt-0.5"></div>
+            </div>
+            <div className="block sm:hidden">
+              <div className={`${italiana.className} text-black`}>
+                  <div className="text-6xl">
+                    GENERATE 
+                  </div>
+                  <div className="relative w-full h-[30px]">
+                    <div className="w-full h-[4px] top-2.5 bg-[#5C724A] absolute z-0"></div>
+                    <div className="w-full h-[4px] absolute  left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-[18px] text-black tracking-[0.724em] z-10">
+                      WELCOME TO MY PAGE
+                    </div>
+                  </div>
+                  <div className="text-6xl">
+                    YOUR TODAY
+                  </div>
+              </div>
+            </div>
+            <div className="hidden sm:block">
+              <div className={`${italiana.className} text-7xl text-black`}>
+                 GENERATE YOUR TODAY
+              </div>
+              <div>
+                <div className="w-full h-[4px] bg-[#5C724A] mt-0.5"></div>
+                <div className={` text-4xl text-black instrument-serif-regular`}>WELCOME TO MY PAGE</div>
+              </div>
+            </div>
           </div>
-          <div className="bg-amber-600 mt-1 w-full h-[35vh]">
-
+          <div className="block sm:hidden mt-1 w-full h-[35vh] flex justify-center items-center opacity-[88%] mix-blend-luminosity">
+            <img src="./images/profile.png" alt="" className="rounded-3xl"/>
+          </div>
+          <div className="hidden sm:block">
+            <div className="mt-1 w-full h-[35vh] flex justify-center items-center opacity-[88%] mix-blend-luminosity">
+              <img src="./images/profile.png" alt="" className="rounded-3xl"/>
+            </div>
+            <div></div>
           </div>
           <div className="bg-amber-600 mt-1 w-full h-[30vh]">
 
