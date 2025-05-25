ee655fd856aeab04f5c7ef5e7b1b6d466053b57d
Ankit-Dalei <ankitdalei123@gmail.com>
2025-05-26 02:17:47 +0530

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,10 +11,10 @@ export default function Home() {
         <div className="h-auto w-screen p-2">
           <div className="w-full h-[8vh] flex justify-between items-center">
             <div className="w-[50px]">            
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" width="40" height="20">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" width="45" height="20">
               <path d="M0 96C0 78.3 14.3 64 32 64l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 128C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 288c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32L32 448c-17.7 0-32-14.3-32-32s14.3-32 32-32l384 0c17.7 0 32 14.3 32 32z"/></svg>
             </div>
-            <div className={`${jacques.className} text-3xl text-black`}>
+            <div className={`${jacques.className} text-4xl text-black`}>
               Ankit Dalei
             </div>
           </div>
