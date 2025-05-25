43a3cb779ce305a82cec243fc2862226377c5a76
Ankit-Dalei <ankitdalei123@gmail.com>
2025-05-26 01:29:14 +0530

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,27 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <div>
-        <div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
+      <div className="bg-[#ffffe4] h-auto w-auto">
+        <div className="h-auto w-screen p-2">
+          <div className="bg-amber-500 w-full h-[8vh]">
+            <div className="w-[50px]">            
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" width="50" height="50">
+              <path d="M0 96C0 78.3 14.3 64 32 64l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 128C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 288c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32L32 448c-17.7 0-32-14.3-32-32s14.3-32 32-32l384 0c17.7 0 32 14.3 32 32z"/></svg>
+            </div>
+            <div></div>
+          </div>
+          <div className="bg-amber-600 mt-1 w-full h-[16vh]">
+
+          </div>
+          <div className="bg-amber-600 mt-1 w-full h-[35vh]">
+
+          </div>
+          <div className="bg-amber-600 mt-1 w-full h-[30vh]">
+
+          </div>
+          <div className="bg-amber-600 mt-1 w-full h-[10vh]">
+
+          </div>
         </div>
         <div></div>
         <div></div>
