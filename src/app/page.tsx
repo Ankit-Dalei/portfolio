d6fb023f4a991fbfe2a4ba17bf9c83d445a9eeee
Ankit-Dalei <ankitdalei123@gmail.com>
2025-06-01 01:57:07 +0530

    commit destop pre final design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -324,7 +324,7 @@ export default function Home() {
         <div className="h-auto w-screen p-2 flex flex-col justify-center items-center mt-2">
           <h2 className="text-black text-2xl font-serif">Vlogs</h2>
           <div className=" flex h-auto w-[100%] flex-col justify-between items-center mt-3">
-            <div className="h-[170px] w-[100%] relative sm:h-[350px] md:h-[420px]">
+            <div className="h-[250px] w-[100%] relative sm:h-[350px] md:h-[420px]">
               <Link href={`https://weather-ten-sigma-87.vercel.app/`} target="_blank">
                 <Image
                 src="/proimg.png"
@@ -334,7 +334,7 @@ export default function Home() {
                 />
               </Link>
             </div>
-            <div className="h-[170px] w-[100%] relative mt-2 sm:h-[350px] md:h-[420px]">
+            <div className="h-[250px] w-[100%] relative mt-2 sm:h-[350px] md:h-[420px]">
               <Link href={`https://portfolio-nu-bice-89.vercel.app/`} target="_blank">
                 <Image
                 src="/proimg2.png"
@@ -352,7 +352,7 @@ export default function Home() {
         <div className="h-auto w-screen p-2 flex flex-col justify-center items-center mt-1">
           <h2 className="text-black text-2xl font-serif">Projects</h2>
           <div className=" flex h-auto w-[100%] flex-col justify-between items-center mt-3">
-            <div className="h-[170px] w-[100%] relative sm:h-[350px] md:h-[420px]">
+            <div className="h-[250px] w-[100%] relative sm:h-[350px] md:h-[420px]">
               <Link href={`https://weather-ten-sigma-87.vercel.app/`} target="_blank">
                 <Image
                 src="/proimg.png"
@@ -362,7 +362,7 @@ export default function Home() {
                 />
               </Link>
             </div>
-            <div className="h-[170px] w-[100%] relative mt-2 sm:h-[350px] md:h-[420px]">
+            <div className="h-[250px] w-[100%] relative mt-2 sm:h-[350px] md:h-[420px]">
               <Link href={`https://portfolio-nu-bice-89.vercel.app/`} target="_blank">
                 <Image
                 src="/proimg2.png"
@@ -629,8 +629,177 @@ export default function Home() {
             </div>
           </div>
         </div>
-        <div className="lg:h-[100vh] lg:w-full lg:p-2 bg-amber-500">
-
+        <div className="lg:h-auto lg:w-full lg:p-2 lg:mt-7">
+          <div className="lg:w-full lg:flex lg:justify-between">
+            <div className="lg:w-[33%] xl:w-[420px] lg:h-[600px] lg:flex lg:flex-col lg:justify-between lg:items-center">
+              <div className="lg:rounded-2xl lg:w-full lg:h-[80px] lg:flex lg:justify-between lg:items-top">
+                <div className="h-[50px] w-[50px] rounded-3xl relative flex justify-center items-center">
+                  <Link href={"https://www.linkedin.com/in/ankit-dalei-0aab70161/"}>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" width={40} height={40}>
+                    <path d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z"/></svg>
+                  </Link>
+                </div>
+                <div className="h-[50px] w-[50px] rounded-3xl relative flex justify-center items-center">
+                  <Link href={""}>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" width={40} height={40}>
+                    <path d="M448 96c0-35.3-28.7-64-64-64H64C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V96zM265.8 407.7c0-1.8 0-6 .1-11.6c.1-11.4 .1-28.8 .1-43.7c0-15.6-5.2-25.5-11.3-30.7c37-4.1 76-9.2 76-73.1c0-18.2-6.5-27.3-17.1-39c1.7-4.3 7.4-22-1.7-45c-13.9-4.3-45.7 17.9-45.7 17.9c-13.2-3.7-27.5-5.6-41.6-5.6s-28.4 1.9-41.6 5.6c0 0-31.8-22.2-45.7-17.9c-9.1 22.9-3.5 40.6-1.7 45c-10.6 11.7-15.6 20.8-15.6 39c0 63.6 37.3 69 74.3 73.1c-4.8 4.3-9.1 11.7-10.6 22.3c-9.5 4.3-33.8 11.7-48.3-13.9c-9.1-15.8-25.5-17.1-25.5-17.1c-16.2-.2-1.1 10.2-1.1 10.2c10.8 5 18.4 24.2 18.4 24.2c9.7 29.7 56.1 19.7 56.1 19.7c0 9 .1 21.7 .1 30.6c0 4.8 .1 8.6 .1 10c0 4.3-3 9.5-11.5 8C106 393.6 59.8 330.8 59.8 257.4c0-91.8 70.2-161.5 162-161.5s166.2 69.7 166.2 161.5c.1 73.4-44.7 136.3-110.7 158.3c-8.4 1.5-11.5-3.7-11.5-8zm-90.5-54.8c-.2-1.5 1.1-2.8 3-3.2c1.9-.2 3.7 .6 3.9 1.9c.3 1.3-1 2.6-3 3c-1.9 .4-3.7-.4-3.9-1.7zm-9.1 3.2c-2.2 .2-3.7-.9-3.7-2.4c0-1.3 1.5-2.4 3.5-2.4c1.9-.2 3.7 .9 3.7 2.4c0 1.3-1.5 2.4-3.5 2.4zm-14.3-2.2c-1.9-.4-3.2-1.9-2.8-3.2s2.4-1.9 4.1-1.5c2 .6 3.3 2.1 2.8 3.4c-.4 1.3-2.4 1.9-4.1 1.3zm-12.5-7.3c-1.5-1.3-1.9-3.2-.9-4.1c.9-1.1 2.8-.9 4.3 .6c1.3 1.3 1.8 3.3 .9 4.1c-.9 1.1-2.8 .9-4.3-.6zm-8.5-10c-1.1-1.5-1.1-3.2 0-3.9c1.1-.9 2.8-.2 3.7 1.3c1.1 1.5 1.1 3.3 0 4.1c-.9 .6-2.6 0-3.7-1.5zm-6.3-8.8c-1.1-1.3-1.3-2.8-.4-3.5c.9-.9 2.4-.4 3.5 .6c1.1 1.3 1.3 2.8 .4 3.5c-.9 .9-2.4 .4-3.5-.6zm-6-6.4c-1.3-.6-1.9-1.7-1.5-2.6c.4-.6 1.5-.9 2.8-.4c1.3 .7 1.9 1.8 1.5 2.6c-.4 .9-1.7 1.1-2.8 .4z"/></svg>
+                  </Link>
+                </div>
+                <div className="h-[50px] w-[50px] rounded-3xl relative flex justify-center items-center">
+                  <Link href={""}>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" width={40} height={40}>
+                    <path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"/></svg>
+                  </Link>
+                </div>
+                <div className="h-[50px] w-[50px] rounded-3xl relative flex justify-center items-center">
+                  <Link href={""}>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" width={40} height={40}>
+                    <path d="M64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64zm297.1 84L257.3 234.6 379.4 396H283.8L209 298.1 123.3 396H75.8l111-126.9L69.7 116h98l67.7 89.5L313.6 116h47.5zM323.3 367.6L153.4 142.9H125.1L296.9 367.6h26.3z"/></svg>
+                  </Link>
+                </div>
+                <div className="h-[50px] w-[50px] rounded-3xl relative flex justify-center items-center">
+                  <Link href={""}>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width={40} height={40}>
+                    <path d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48L48 64zM0 176L0 384c0 35.3 28.7 64 64 64l384 0c35.3 0 64-28.7 64-64l0-208L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z"/></svg>
+                  </Link>
+                </div>
+                <div className="h-[50px] w-[50px] rounded-3xl relative flex justify-center items-center">
+                  <Link href={""}>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width={40} height={40}>
+                    <path d="M469.3 19.3l23.4 23.4c25 25 25 65.5 0 90.5l-56.4 56.4L322.3 75.7l56.4-56.4c25-25 65.5-25 90.5 0zM44.9 353.2L299.7 98.3 413.7 212.3 158.8 467.1c-6.7 6.7-15.1 11.6-24.2 14.2l-104 29.7c-8.4 2.4-17.4 .1-23.6-6.1s-8.5-15.2-6.1-23.6l29.7-104c2.6-9.2 7.5-17.5 14.2-24.2zM249.4 103.4L103.4 249.4 16 161.9c-18.7-18.7-18.7-49.1 0-67.9L94.1 16c18.7-18.7 49.1-18.7 67.9 0l19.8 19.8c-.3 .3-.7 .6-1 .9l-64 64c-6.2 6.2-6.2 16.4 0 22.6s16.4 6.2 22.6 0l64-64c.3-.3 .6-.7 .9-1l45.1 45.1zM408.6 262.6l45.1 45.1c-.3 .3-.7 .6-1 .9l-64 64c-6.2 6.2-6.2 16.4 0 22.6s16.4 6.2 22.6 0l64-64c.3-.3 .6-.7 .9-1L496 350.1c18.7 18.7 18.7 49.1 0 67.9L417.9 496c-18.7 18.7-49.1 18.7-67.9 0l-87.4-87.4L408.6 262.6z"/></svg>
+                  </Link>
+                </div>
+              </div>
+              <div className=" lg:rounded-2xl lg:w-full lg:h-auto">
+                <h2 className="text-black text-2xl font-serif flex justify-center items-center">Skills</h2>
+                <div className="h-auto w-full mt-1 grid grid-cols-6 gap-4 justify-items-center items-center">
+                  {socialLinks.map((item) => (
+                    <div
+                      key={item.id}
+                      className="h-[50px] w-[50px] rounded-3xl relative flex justify-center items-center"
+                    >
+                      {/* <Link href={item.url} target="_blank"> */}
+                        {item.svgPath}
+                      {/* </Link> */}
+                    </div>
+                  ))}
+                </div>
+              </div>
+              <div className="lg:rounded-2xl lg:w-full lg:h-[350px] mt-3">
+                <div className="lg:text-black lg:font-serif lg:flex lg:justify-center lg:items-center lg:text-2xl">Vlogs</div>
+                <div className="lg:w-full lg:h-[70%] rounded-2xl relative mt-2">
+                  <Link href={`https://weather-ten-sigma-87.vercel.app/`} target="_blank">
+                    <Image
+                    src="/proimg.png"
+                    alt="profileImage"
+                    fill
+                    className="rounded-xl object-cover"
+                    />
+                  </Link>
+                </div>
+                <div className="lg:w-full lg:flex lg:justify-end lg:items-center lg:text-black lg:text-xl lg:font-serif mt-2">
+                  <Link href={`vlog`}>
+                    See More
+                  </Link>
+                </div>
+              </div>
+            </div>
+            <div className="lg:w-[31%] xl:w-[400px] lg:h-[600px] bg-[#a69494] rounded-b-full p-6 text-center overflow-y-auto">
+              <h1 className="text-3xl font-bold tracking-widest text-black mb-6 border-b border-black inline-block pb-1">
+                ABOUT ME
+              </h1>
+              <p className="lg:text-[11.5px] md:text-[13px] italic text-justify text-[#3e3e3e] leading-relaxed font-[cursive]">
+                I am a passionate and dedicated Full Stack Developer 
+                with a strong command of the MERN stack 
+                (MongoDB, Express.js, React.js, Node.js) 
+                and solid experience in JavaScript and C++. 
+                I specialize in building responsive and 
+                user-friendly web applications, ensuring 
+                high performance and clean code architecture. 
+                My work extends beyond typical development—I’ve 
+                successfully integrated real-time data from IoT 
+                devices such as ESP8266, DHT22, LDR, and heart 
+                rate sensors, enhancing the interactivity and 
+                functionality of my projects.<br/>
+                I have hands-on experience in both front-end 
+                and back-end development and enjoy tackling 
+                complex challenges to deliver scalable solutions. 
+                Currently, I am diving deeper into industrial 
+                automation, sensor data analytics, and 
+                machine learning applications to expand my 
+                technical horizons. I am a quick learner, 
+                a creative problem-solver, and thrive in 
+                collaborative, fast-paced environments. 
+                My goal is to contribute to innovative 
+                tech-driven projects that make a real-world impact.
+              </p>
+            </div>
+            <div className="lg:w-[33%] xl:w-[420px] lg:h-[600px] lg:flex lg:flex-col lg:justify-between lg:items-center">
+              <div className="lg:w-full lg:h-[95%] relative">
+                <Link href={`https://weather-ten-sigma-87.vercel.app/`} target="_blank">
+                  <Image
+                  src="/designimg.png"
+                  alt="profileImage"
+                  fill
+                  />
+                  </Link>
+              </div>
+              <div className="lg:w-full lg:flex lg:justify-end lg:items-center lg:text-black lg:text-xl lg:font-serif">
+                <Link href={`vlog`}>
+                  See More
+                </Link>
+              </div>
+            </div>
+          </div>
+          <div className="lg:h-auto lg:w-full lg:flex lg:flex-col lg:mt-15">
+            <div className="lg:w-full lg:flex lg:justify-between lg:items-center">
+              <div className="lg:w-[350px] lg:h-[200px] rounded-2xl relative">
+                <Link href={`https://weather-ten-sigma-87.vercel.app/`} target="_blank">
+                  <Image
+                  src="/proimg.png"
+                  alt="profileImage"
+                  fill
+                  className="rounded-xl object-cover"
+                  />
+                </Link>
+              </div>
+              <div className="lg:w-[350px] lg:h-[200px] rounded-2xl relative">
+                <Link href={`https://weather-ten-sigma-87.vercel.app/`} target="_blank">
+                  <Image
+                  src="/proimg.png"
+                  alt="profileImage"
+                  fill
+                  className="rounded-xl object-cover"
+                  />
+                </Link>
+              </div>
+              <div className="lg:w-[350px] lg:h-[200px] rounded-2xl relative">
+                <Link href={`https://weather-ten-sigma-87.vercel.app/`} target="_blank">
+                  <Image
+                  src="/proimg.png"
+                  alt="profileImage"
+                  fill
+                  className="rounded-xl object-cover"
+                  />
+                </Link>
+              </div>
+              <div className="lg:w-[350px] lg:h-[200px] rounded-2xl relative">
+                <Link href={`https://weather-ten-sigma-87.vercel.app/`} target="_blank">
+                  <Image
+                  src="/proimg.png"
+                  alt="profileImage"
+                  fill
+                  className="rounded-xl object-cover"
+                  />
+                </Link>
+              </div>
+            </div>
+            <div className="lg:w-full lg:flex lg:justify-end lg:items-center lg:text-black lg:text-xl lg:font-serif">
+              <Link href={`project`}>
+                See More
+              </Link>
+            </div>
+          </div>
         </div>
         <div className="lg:h-[5vh] lg:w-full lg:p-2 bg-gray-800 lg:flex lg:justify-between lg:items-center lg:font-serif">
           <div>Contact Via Email</div>
