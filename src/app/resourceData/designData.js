86260452095aad4db7b4a2dd4ce84de5e8923252
Ankit-Dalei <ankitdalei123@gmail.com>
2025-06-03 20:38:29 +0530

    push

--- src/app/resourceData/designData.js
+++ src/app/resourceData/designData.js
@@ -4,61 +4,7 @@ const designData = [
     name: "Weather App",
     image: "/designimg.png",
     url: ""
-  },
-  {
-    id: 2,
-    name: "MedGuide",
-    image: "/designimg.png",
-    url: ""
-  },
-  {
-    id: 3,
-    name: "Tic-Tac-Toe",
-    image: "/designimg.png",
-    url: ""
-  },
-  {
-    id: 4,
-    name: "Todo",
-    image: "/designimg.png",
-    url: ""
-  },
-  {
-    id: 5,
-    name: "Portfolio",
-    image: "/designimg.png",
-    url: ""
-  },
-  {
-    id: 6,
-    name: "Portfolio",
-    image: "/designimg.png",
-    url: ""
-  },
-  {
-    id: 7,
-    name: "Portfolio",
-    image: "/designimg.png",
-    url: ""
-  },
-  {
-    id: 8,
-    name: "Portfolio",
-    image: "/designimg.png",
-    url: ""
-  },
-  {
-    id: 9,
-    name: "Portfolio",
-    image: "/designimg.png",
-    url: ""
-  },
-  {
-    id: 10,
-    name: "Portfolio",
-    image: "/designimg.png",
-    url: ""
-  },
+  }
 ];
 
 export default designData;
